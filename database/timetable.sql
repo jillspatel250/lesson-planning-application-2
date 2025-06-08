f085102a97e6b28967007edab955e4c11fb45b7b
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2025-06-08 13:04:52 +0530

    Revert "Timetable & attendance management"
    This reverts commit 877135f8e3e2b754f3234e92d3ee8b15ef198811.

--- database/timetable.sql
+++ database/timetable.sql
@@ -1,17 +1,12 @@
 create table public.timetable (
-  id uuid not null default gen_random_uuid (),
-  day text null,
-  type text null,
-  subject uuid null,
-  faculty uuid null,
-  department uuid null,
-  "to" timestamp with time zone null,
-  "from" timestamp with time zone null,
-  division text null,
-  batch text null,
-  sem integer null,
-  constraint Time Table_pkey primary key (id),
-  constraint Time Table_subject_fkey foreign KEY (subject) references subjects (id),
-  constraint Time Table_faculty_fkey foreign KEY (faculty) references users (id),
-  constraint Time Table_department_fkey foreign KEY (department) references departments (id)
-) TABLESPACE pg_default;
+  id uuid primary key default gen_random_uuid(),
+  date timestamptz not null,
+  type text not null,
+  subject uuid not null,
+  faculty uuid not null,
+  department uuid not null,
+  "to" timestamptz not null,
+  "from" timestamptz not null,
+  division text not null,
+  batch text not null
+);
